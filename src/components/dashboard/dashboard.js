b48a721d5e6e49e3bd146d71f4f2b157afd3a461
vibhorAgarwal <vibhoragarwal199@gmail.com>
2022-06-12 13:50:28 +0530

    user reducer added for restricting the pages

---
+++ src/components/dashboard/dashboard.js
@@ -0,0 +1,11 @@
+import { Outlet, Link } from "react-router-dom";
+export default function Dashboard() {
+
+    return (
+        <>
+            <p>
+                Dashboard
+            </p>
+        </>
+    )
+}
---
+++ src/redux/user/reducer.js
@@ -0,0 +1,14 @@
+import { isTokenExist } from '../../utils/authTokenStorage';
+
+const initialState = {
+  isAuth: isTokenExist()
+};
+
+const userReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
+
+export default userReducer;
